65454f0e16df4b71efecbf5ce9804557ad8bf540
Samrude1 <126822763+Samrude1@users.noreply.github.com>
2025-05-16 21:56:13 +0300

    Flippin cards
    flip cards with CSS

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/image/helmet-1.png" },
-  { src: "/image/potion-1.png" },
-  { src: "/image/ring-1.png" },
-  { src: "/image/scroll-1.png" },
-  { src: "/image/shield-1.png" },
-  { src: "/image/sword-1.png" },
+  { src: "/image/helmet-1.png", matched: false },
+  { src: "/image/potion-1.png", matched: false },
+  { src: "/image/ring-1.png", matched: false },
+  { src: "/image/scroll-1.png", matched: false },
+  { src: "/image/shield-1.png", matched: false },
+  { src: "/image/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -32,6 +32,35 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
@@ -39,7 +68,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard handleChoice={handleChoice} card={card} key={card.id} />
+          <SingleCard
+            handleChoice={handleChoice}
+            card={card}
+            key={card.id}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- memory-game/src/components/SingleCard.jsx
+++ memory-game/src/components/SingleCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./SingleCard.css";
 
-const SingleCard = ({ card, handleChoice }) => {
+const SingleCard = ({ card, handleChoice, flipped }) => {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img src={card.src} alt="card front" className="front" />
         <img
           src="/image/cover.png"
